0c7998ecbf726347941b968e031fe8da8949af13
ajilkumar <ajilk0325@gmail.com>
2026-01-24 23:53:45 +0530

    Updated pipeline for working properly

--- services/worker/src/config/env.ts
+++ services/worker/src/config/env.ts
@@ -4,6 +4,7 @@ import * as z from "zod";
 const eventSchema = z.object({
   DATABASE_URL: z.string(),
   RABBITMQ_URL: z.string(),
+  FAILURE_PROBABILITY: z.coerce.number().min(0).max(1).default(0),
 });
 
 
